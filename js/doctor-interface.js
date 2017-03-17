1a13e87dc725a81a46e9d132c1e9f0708b0c13e7
Maggie Harrington <maggie.harrington@gmail.com>
2017-03-17 16:44:06 -0700

    refactor to use promises

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -8,11 +8,12 @@ $(document).ready(function() {
     console.log(apiKey);
     $('#confirm-input').text('You searched for doctors who can treat ' + medicalIssue + '.');
     $('#results').append('<h3>Here are your results:</h3><br>');
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey,
-    function(response) {
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+    .then(function(response) {
       console.log(response);
 
       for (i = 0; i < response.data.length; i ++) {
+        // for (j = 0; j < response.data[i].specialties.length; j ++) {
         $('#results').append(
           '<h4>' + response.data[i].profile.first_name + ' ' +
           response.data[i].profile.middle_name +  ' ' +
@@ -32,7 +33,11 @@ $(document).ready(function() {
           '<p>' + response.data[i].practices[0].phones[0].number + ', ' +
           response.data[i].practices[0].phones[0].type + '</p><br><br>'
         );
+        // }
       }
+    })
+    .fail(function(response) {
+      $('#results').append(error.responseJSON.message);
     });
   });
 });

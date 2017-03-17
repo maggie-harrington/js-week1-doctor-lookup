65370f7d3e50e7d18fe77dcd4e53774f32ba0356
Maggie Harrington <maggie.harrington@gmail.com>
2017-03-17 14:59:53 -0700

    require apiKey in js file, add ajax request and confirm that response objects are returned by printing in console

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,10 +1,17 @@
+var apiKey = require('./../.env').apiKey;
+
 $(document).ready(function() {
   $('#input-issue').submit(function() {
     event.preventDefault();
+    $('#results').empty();
     var medicalIssue = $('#medical-issue').val();
-    console.log(medicalIssue);
+    console.log(apiKey);
     $('#confirm-input').text('You searched for doctors who can treat ' + medicalIssue + '.');
     $('#results').append('<h4>Here are your results:</h4><br>');
-    $('#results').append('<li>' + 'results here' + '</li>');
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey,
+    function(response) {
+      console.log(response);
+      $('#results').append('<li>' + 'results here' + '</li>');
+    });
   });
 });

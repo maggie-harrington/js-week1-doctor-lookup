179f4a8b1d1f68e69b033376bf8475e1230193d2
Maggie Harrington <maggie.harrington@gmail.com>
2017-03-17 16:10:16 -0700

    add for loop to display all returned doctors in array

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -11,25 +11,28 @@ $(document).ready(function() {
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey,
     function(response) {
       console.log(response);
-      $('#results').append(
-        '<h4>' + response.data[0].profile.first_name + ' ' +
-        response.data[0].profile.middle_name +  ' ' +
-        response.data[0].profile.last_name +  ', ' +
-        response.data[0].profile.title + '</h4>' +
 
-        '<p>' + response.data[0].specialties[0].name + '</p>' +
-        
-        '<p>' + response.data[0].practices[0].name + '</p>' +
+      for (i = 0; i < response.data.length; i ++) {
+        $('#results').append(
+          '<h4>' + response.data[i].profile.first_name + ' ' +
+          response.data[i].profile.middle_name +  ' ' +
+          response.data[i].profile.last_name +  ', ' +
+          response.data[i].profile.title + '</h4>' +
 
-        '<p>' + response.data[0].practices[0].visit_address.street + '<br>' +
-        response.data[0].practices[0].visit_address.street2 + '<br>' +
-        response.data[0].practices[0].visit_address.city + ', ' +
-        response.data[0].practices[0].visit_address.state + ' ' +
-        response.data[0].practices[0].visit_address.zip + '</p>' +
+          '<p>' + response.data[i].specialties[0].name + '</p>' +
 
-        '<p>' + response.data[0].practices[0].phones[0].number + ', ' +
-        response.data[0].practices[0].phones[0].type + '</p><br><br>'
-      );
+          '<p>' + response.data[i].practices[0].name + '</p>' +
+
+          '<p>' + response.data[i].practices[0].visit_address.street + '<br>' +
+          response.data[i].practices[0].visit_address.street2 + '<br>' +
+          response.data[i].practices[0].visit_address.city + ', ' +
+          response.data[i].practices[0].visit_address.state + ' ' +
+          response.data[i].practices[0].visit_address.zip + '</p>' +
+
+          '<p>' + response.data[i].practices[0].phones[0].number + ', ' +
+          response.data[i].practices[0].phones[0].type + '</p><br><br>'
+        );
+      }
     });
   });
 });

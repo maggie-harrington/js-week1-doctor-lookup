a2650965ed08cf0376a3b45bd5a907d24623529b
Maggie Harrington <maggie.harrington@gmail.com>
2017-03-23 18:01:55 -0700

    refactor using callback function to completely separate back- and front-end

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,8 +1,31 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var callback = function(response) {
+  for (i = 0; i < response.data.length; i ++) {
+    $('#results').append(
+      '<h4>' + response.data[i].profile.first_name + ' ' +
+      response.data[i].profile.middle_name +  ' ' +
+      response.data[i].profile.last_name +  ', ' +
+      response.data[i].profile.title + '</h4>' +
+
+      '<p>' + response.data[i].specialties[0].name + '</p>' +
+
+      '<p>' + response.data[i].practices[0].name + '</p>' +
+
+      '<p>' + response.data[i].practices[0].visit_address.street + '<br>' +
+      response.data[i].practices[0].visit_address.street2 + '<br>' +
+      response.data[i].practices[0].visit_address.city + ', ' +
+      response.data[i].practices[0].visit_address.state + ' ' +
+      response.data[i].practices[0].visit_address.zip + '</p>' +
+
+      '<p>' + response.data[i].practices[0].phones[0].number + ', ' +
+      response.data[i].practices[0].phones[0].type + '</p><br><br>'
+    );
+  }
+}
+
 $(document).ready(function() {
   var doctorObject = new Doctor();
-  doctorObject.getDoctors();
 
   $('#input-issue').submit(function() {
     event.preventDefault();
@@ -10,6 +33,6 @@ $(document).ready(function() {
     var medicalIssue = $('#medical-issue').val();
     $('#confirm-input').text('You searched for doctors who can treat ' + medicalIssue + '.');
     $('#results').append('<h3>Here are your results:</h3><br>');
-    doctorObject.getDoctors(medicalIssue);
+    doctorObject.getDoctors(medicalIssue, callback);
   });
 });
